7fb8082d8b12aca03325687cfbb087535174c083
kristijan-kresic-hvar <kristijan.kresic.dev@gmail.com>
2022-12-12 20:56:09 +0100

    Initial navbar setup

--- src/components/NavItem/NavItem.jsx
+++ src/components/NavItem/NavItem.jsx
@@ -1,11 +1,25 @@
+import PropTypes from 'prop-types'
+import { NavLink } from 'react-router-dom'
+
 import styles from './NavItem.module.scss'
 
-const NavItem = () => {
+const NavItem = ({ text, to, icon, ...props }) => {
     return (
-        <div className={styles.nav_item}>
-            NavItem
-        </div>
+        <NavLink
+            className={styles.nav_item}
+            to={to}
+            {...props}
+        >
+            {text}
+            {icon && <img src={icon} alt="icon illustration" />}
+        </NavLink>
     )
+}
+
+NavItem.propTypes = {
+    text: PropTypes.string.isRequired,
+    to: PropTypes.string.isRequired,
+    icon: PropTypes.string
 }
 
 export default NavItem
