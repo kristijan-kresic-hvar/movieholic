50b65b1314197e08ec905d59852771c1b95ef8f8
Kristijan Krešić <52584740+kristijan-kresic-hvar@users.noreply.github.com>
2022-12-14 07:01:12 +0100

    Changed ui of the navbar, added link to logo image

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import styles from './Navbar.module.scss'
 
+import { Link } from 'react-router-dom'
+
 import NavItem from '../NavItem/NavItem'
 
 import logoSVG from '../../assets/img/logo.svg'
@@ -8,20 +10,24 @@ import arrowRightSVG from '../../assets/icons/arrow_right.svg'
 const Navbar = () => {
     return (
         <div className={styles.navbar}>
-            <div className={styles.navbar__logo}>
-                <img src={logoSVG} alt="logo illustration" />
-            </div>
-            <div className={styles.navbar__items}>
-                <NavItem
-                    text="Movies"
-                />
-                <NavItem
-                    text="TV Shows"
-                />
-                <NavItem
-                    text="Login"
-                    icon={arrowRightSVG}
-                />
+            <div className={styles.navbar__inner}>
+                <Link to="/">
+                    <div className={styles.navbar__logo}>
+                        <img src={logoSVG} alt="logo illustration" />
+                    </div>
+                </Link>
+                <div className={styles.navbar__items}>
+                    <NavItem
+                        text="Movies"
+                    />
+                    <NavItem
+                        text="TV Shows"
+                    />
+                    <NavItem
+                        text="Login"
+                        icon={arrowRightSVG}
+                    />
+                </div>
             </div>
         </div>
     )
